93b5bfd9529ff3ad8046944e55ded339e7b5c5b5
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-04-24 15:19:09 +0300

    базовая работа с полями, редактировнаие, добавление, удаление

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,24 +1,47 @@
-import { Admin, List, Datagrid, TextField, DateField, EditButton, DeleteButton, Resource } from "react-admin";
+import { Admin, List, Datagrid, TextField, DateField, EditButton, DeleteButton, Resource, Create, SimpleForm, TextInput, DateInput, Edit } from "react-admin";
 
 import restProvider from "ra-data-json-server";
 
-const PostList = () => {
+const PostList = (props) => {
     return (
-        <List>
+        <List {...props}>
             <Datagrid>
                 <TextField source="id" />
                 <TextField source="title" />
-                <DateField source="publishedhAt" />
-                <EditButton basePath="/posts" />
-                <DeleteButton basePath="/posts" />
+                <DateField source="publishedhAt" label="Дата" />
+                <EditButton basePath="/posts" label="Редактировать" />
+                <DeleteButton basePath="/posts" label="Удалить" />
             </Datagrid>
         </List>
     );
 };
 
+const PostCreate = (props) => {
+    return (
+        <Create {...props}>
+            <SimpleForm>
+                <TextInput source="title" />
+                <DateInput source="publishedhAt" label="Published" />
+            </SimpleForm>
+        </Create>
+    );
+};
+
+const PostEdit = (props) => {
+    return (
+        <Edit {...props}>
+            <SimpleForm>
+                <TextInput source="id" disabled />
+                <TextInput source="title" />
+                <DateInput source="publishedhAt" label="Published" />
+            </SimpleForm>
+        </Edit>
+    );
+};
+
 const AdminPage = () => (
     <Admin dataProvider={restProvider("http://localhost:3000")} basename="/admin">
-        <Resource name="posts" list={PostList} />
+        <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} />
     </Admin>
 );
 
