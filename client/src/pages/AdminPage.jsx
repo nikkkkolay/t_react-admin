086a88451c754d41231beb9ab4a60902e85d5f1f
turboreaktivnayatyaga <ggn.f764@gmail.com>
2024-04-28 22:56:40 +0300

    кастомный бекграунд для страницы логина

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,4 +1,4 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource, Login } from "react-admin";
 import polyglotI18nProvider from "ra-i18n-polyglot";
 import russianMessages from "ra-language-russian";
 import NewspaperIcon from "@mui/icons-material/Newspaper";
@@ -11,8 +11,10 @@ import { PostList, PostCreate, PostEdit, DocsList, DocsCreate, DocsEdit, Contact
 
 const i18nProvider = polyglotI18nProvider(() => russianMessages, "ru");
 
+const MyLoginPage = () => <Login backgroundImage="https://source.unsplash.com/random/1600x900/daily" />;
+
 const AdminPage = () => (
-    <Admin dataProvider={myDataProvider} basename="/admin" i18nProvider={i18nProvider} authProvider={authProvider}>
+    <Admin loginPage={MyLoginPage} dataProvider={myDataProvider} basename="/admin" i18nProvider={i18nProvider} authProvider={authProvider}>
         <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} options={{ label: "Новости" }} icon={NewspaperIcon} />
         <Resource name="documents" list={DocsList} create={DocsCreate} edit={DocsEdit} options={{ label: "Документы" }} icon={HistoryEduIcon} />
         <Resource name="contacts" list={Contacts} edit={Contacts} options={{ label: "Контакты" }} icon={CallIcon} />
